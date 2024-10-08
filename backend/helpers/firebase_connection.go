783abf2c68c7e5add8407c4e69dba5f1bde3b104
kiranlal <kiranlal8493@gmail.com>
2024-10-08 18:05:49 +0530

    bug

--- backend/helpers/firebase_connection.go
+++ backend/helpers/firebase_connection.go
@@ -13,7 +13,7 @@ var FirebaseApp *firebase.App
 
 func InitFirebase() {
 	var err error
-	opt := option.WithCredentialsFile("sparrowdine-7dca2-firebase-adminsdk-qu31m-c913048e8c.json")
+	opt := option.WithCredentialsFile("config.json")
 	FirebaseApp, err = firebase.NewApp(context.Background(), nil, opt)
 	if err != nil {
 		log.Fatal(err)
